62717d4876bc23584ffa2991b68d20e4c6f54425
peplak <62644538+peplak@users.noreply.github.com>
2024-10-11 15:14:32 +0000

    сделал рабочую форму контактов

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -1,56 +1,58 @@
 import React from 'react';
 import { useForm, ValidationError } from '@formspree/react';
-import './Contacts.css'; // Используем для стилизации
+import './Contacts.css'; // Подключаем стили
 
-function ContactForm() {
+function Contacts() {
   const [state, handleSubmit] = useForm("mgveewdq");
-
   if (state.succeeded) {
-    return <p>Спасибо за ваше сообщение! Мы свяжемся с вами в ближайшее время.</p>;
+      return <p>Спасибо за сообщение!</p>;
   }
-
   return (
-    <form onSubmit={handleSubmit} className="contact-form">
-      <label htmlFor="name">Имя:</label>
-      <input
-        id="name"
-        type="text"
-        name="name"
-        required
-      />
-      <ValidationError prefix="Name" field="name" errors={state.errors} />
-      
-      <label htmlFor="email">Адрес электронной почты:</label>
-      <input
-        id="email"
-        type="email"
-        name="email"
-        required
-      />
-      <ValidationError prefix="Email" field="email" errors={state.errors} />
+    <div className="contact-form-container">
+      <form onSubmit={handleSubmit}>
+        <h2>Свяжитесь с нами!</h2> 
+        
+        <label htmlFor="name">Имя</label>
+        <input
+          id="name"
+          type="text"
+          name="name"
+        />
+        <ValidationError 
+          prefix="Имя" 
+          field="name"
+          errors={state.errors}
+        />
 
-      <label htmlFor="message">Сообщение:</label>
-      <textarea
-        id="message"
-        name="message"
-        required
-      />
-      <ValidationError prefix="Message" field="message" errors={state.errors} />
+        <label htmlFor="email">Адрес электронной почты</label>
+        <input
+          id="email"
+          type="email"
+          name="email"
+        />
+        <ValidationError 
+          prefix="Email" 
+          field="email"
+          errors={state.errors}
+        />
 
-      <button type="submit" disabled={state.submitting}>
-        Отправить сообщение
-      </button>
-    </form>
+        <label htmlFor="message">Сообщение</label>
+        <textarea
+          id="message"
+          name="message"
+        />
+        <ValidationError 
+          prefix="Message" 
+          field="message"
+          errors={state.errors}
+        />
+
+        <button type="submit" disabled={state.submitting}>
+          Отправить
+        </button>
+      </form>
+    </div>
   );
 }
-
-const Contacts = () => {
-  return (
-    <section id="contacts">
-      <h2>Свяжитесь с нами</h2>
-      <ContactForm />
-    </section>
-  );
-};
 
 export default Contacts;
